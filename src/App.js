43bc2502fa0af6ae22933076cd7452dca87e5622
Anisa Ramadhani <anisaramadhani585@gmail.com>
2025-09-30 19:38:56 +0700

    commit pertama dashboard

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from './Component/Navbar';
 import Hero from './Component/Hero';
 import Features from './Component/Features';
 import Team from './Component/Team';
 import About from './Component/About';
-import './style.css'; // ← perbaikan di sini
+import Dashboard from './Component/Dashboard';
+
+import './style.css';
+import './Dashboard.css';
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Hero />
-      <Features />
-      <Team />
-      <About />
-    </div>
+    <Router>
+      <Routes>
+        {/* Landing Page */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar />
+              <Hero />
+              <Features />
+              <Team />
+              <About />
+            </>
+          }
+        />
+
+        {/* Dashboard */}
+        <Route
+          path="/dashboard"
+          element={
+            <Dashboard
+              totalArsip={120}
+              totalArsipHariIni={8}
+              arsipBulanIni={35}
+            />
+          }
+        />
+      </Routes>
+    </Router>
   );
 };
 
